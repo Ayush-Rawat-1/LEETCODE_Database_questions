9db86743b955b3d9f976fc32c85669b5e1872afc
Ayush Rawat <149077649+Ayush-Rawat-1@users.noreply.github.com>
2026-07-02 19:43:46 +0530

    Add SQL query to find invalid IP addresses
    Query to find invalid IP addresses in logs.

---
+++ 3451_find-invalid-ip-addresses.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+
+SELECT ip, COUNT(log_id) AS invalid_count
+FROM logs
+WHERE ip !~ '^([0-9]?|[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])([.]([0-9]?|[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$'
+GROUP BY ip 
+ORDER BY invalid_count DESC, ip DESC
